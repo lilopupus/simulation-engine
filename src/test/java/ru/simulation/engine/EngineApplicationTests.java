096652e29329f8bc3590f48abee926722a9ca722
Makar <Makarstar@yandex.ru>
2023-06-29 11:56:06 +0300

    init test

---
+++ src/test/java/ru/simulation/engine/EngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.simulation.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
